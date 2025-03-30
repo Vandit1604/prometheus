60d751e5db0c75a15992f54cf121bb045456a39d
Vandit Singh <vanditsinghkv@gmail.com>
2025-03-30 21:48:16 +0530

    remove instances of sync.Once around metric registration
    Signed-off-by: Vandit Singh <vanditsinghkv@gmail.com>

--- storage/remote/client.go
+++ storage/remote/client.go
@@ -23,7 +23,6 @@ import (
 	"net/http/httptrace"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/gogo/protobuf/proto"
@@ -100,8 +99,6 @@ var (
 		},
 		[]string{remoteName, endpoint, "response_type"},
 	)
-
-	once sync.Once
 )
 
 // Client allows reading and writing from/to a remote HTTP endpoint.
@@ -144,9 +141,7 @@ type ReadClient interface {
 }
 
 func registerRemoteReadMetrics(registry prometheus.Registerer) {
-	once.Do(func() {
-		registry.MustRegister(remoteReadQueriesTotal, remoteReadQueries, remoteReadQueryDuration)
-	})
+	registry.MustRegister(remoteReadQueriesTotal, remoteReadQueries, remoteReadQueryDuration)
 }
 
 // NewReadClient creates a new client for remote read.
